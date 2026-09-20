42775057ca58ba9e408baf6a2dff7ae251f5ac60
Линк <sm171105@bk.ru>
2026-09-20 22:00:58 +0300

    fix: импорт rememberSaveable в SettingsScreen

--- shared/src/commonMain/kotlin/com/jetbrains/kmpapp/screens/other/SettingsScreen.kt
+++ shared/src/commonMain/kotlin/com/jetbrains/kmpapp/screens/other/SettingsScreen.kt
@@ -64,6 +64,7 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 
 @Composable
